76f8e5b3ddcc7d6c9b708c1d3547a2f21dc573f6
Vernon01 <vernon.swartland@gmail.com>
2016-12-13 17:23:09 +0200

    fix

--- app/src/main/java/com/example/songezo/infoshareapp/services/AudioVisuals/Impl/AudioVisualsServiceImpl.java
+++ app/src/main/java/com/example/songezo/infoshareapp/services/AudioVisuals/Impl/AudioVisualsServiceImpl.java
@@ -9,14 +9,14 @@ import com.example.songezo.infoshareapp.conf.util.App;
 import com.example.songezo.infoshareapp.domain.AudioVisuals;
 import com.example.songezo.infoshareapp.repository.AudioVisuals.AudioVisualsRepository;
 import com.example.songezo.infoshareapp.repository.AudioVisuals.Impl.AudioVisualsImpl;
+import com.example.songezo.infoshareapp.services.AudioVisuals.AudioVisualsService;
 
 import java.util.Set;
 
 /**
  * Created by VERNON on 2016/12/13.
  */
-public class AudioVisualsServiceImpl extends Service implements com.example.songezo.infoshareapp.services.AudioVisuals.AudioVisualsService {
-
+public class AudioVisualsServiceImpl extends Service implements AudioVisualsService{
     private AudioVisualsRepository repository;
 
     private final IBinder localBinder = new AudioVisualsServiceLocalBinder();
